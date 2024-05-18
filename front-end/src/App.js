58b70cd5ab2044dd71d7efd45961e036523a916f
el20614 <el20614@mail.ntua.gr>
2024-05-19 02:41:12 +0300

    chore: Update routing in App.js for better organization and scalability

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Homepage from './Homepage.js' 
 import React from 'react';
-
+//import ReactDOM from 'react-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Login from './Login'; // Make sure to import the Login component
+import SignUp from './Signup'; // Make sure to import the Login component
+ 
 function App() {
   return (
-    <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
